1abc3b5f6ee04f33b906a4c0bd79cefb7443bf7a
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-17 00:53:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  new_key = nil
+  new_key = nil 
   new_value = nil
   name_hash.each do |key,value|
     if new_value == nil || value < new_value
