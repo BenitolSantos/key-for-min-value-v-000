1fe37e2f703f506278fef3539b5cb539f569afd4
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-17 01:06:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_key = nil  #key and value are nil is set because it is replaced.
   min_value = nil
   name_hash.each do |key,value| #we want to iterate over every key.
-    if value == nil || value < min_value  #if the value is nil replace the number.
+    if min_value == nil || value < min_value  #if the value is nil replace the number.
       #min value is stored and compared to the next value which is value.
       min_value = value
       min_key = key
@@ -16,8 +16,8 @@ def key_for_min_value(name_hash)
   min_key
 end
 
-#min_value = nil  <- min_value = 300 
-#then it compares it to 
+#min_value = nil  <- min_value = 300
+#then it compares it to
 
 
 =begin
